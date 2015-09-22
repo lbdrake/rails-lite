d803483cec7d958cf8c378a2123186a09428df73
Leah Drake <drake.leah@gmail.com>
2015-09-22 15:23:35 -0700

    Add cookies

--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -1,22 +1,33 @@
 require 'json'
 require 'webrick'
+require 'byebug'
 
 module Phase4
   class Session
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      cook = req.cookies.find { |cookie| cookie.name == "_rails_lite_app" }
+      # debugger
+      if cook
+        @cookie = JSON.parse(cook.value) #hash key-value pairs
+      else
+        @cookie = {}
+      end
     end
 
     def [](key)
+      @cookie[key]
     end
 
     def []=(key, val)
+      @cookie[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new("_rails_lite_app", JSON.generate(@cookie))
     end
   end
 end
